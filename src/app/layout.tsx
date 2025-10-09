572a9ac3b8d77994865302e56b944003934c9098
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-10-09 12:34:53 -0300

    feat(metadata): Implementar metadata no site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@ import "./globals.css";
 import StyledComponentsRegistry from "@/lib/registry";
 import Header from "@/components/Header/Header";
 import { CartProvider } from "@/context/context";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'FullShop',
+  description: 'Projeto de e-commerce full stack com Next.js, TypeScript e Node.js',
+  icons: {
+    icon: '/shop2.png', 
+  },
+};
 
 export default function RootLayout({
   children,
